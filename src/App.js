19ffcaed7c9accf7011ccc9717ed226072e92d6d
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-26 15:29:17 -0700

    added link to home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import BookList from './components/book/BookList';
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <Switch>
+      <Route exact path="/">
+        <main className="container">
+          <h1>Library Catalog</h1>
+          <BookList />
+        </main>
+      </Route>
+    </Switch>
   );
 }
 
